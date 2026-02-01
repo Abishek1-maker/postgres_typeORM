dc0305ef270db13300d582a4d13b0b20abddf7f4
Abishek kunwar <kesu.kunwar@gmail.com>
2026-02-01 17:15:24 +0545

    Error handling with interceptor (Through this we can catch the error and transform/change to user friendly error )

---
+++ src/interceptors/error/error.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  BadRequestException,
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      catchError((_err) =>
+        throwError(() => new BadRequestException('somethign went wrong')),
+      ),
+    );
+  }
+}
